c6a5ce63704ac44056de576b760b5d8bd649d737
SangHoon <joa3668@gmail.com>
2021-12-13 18:26:18 +0900

    Modify: Destructing styled component prop

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main.js';
 import Nav from './components/Nav/Nav';
+import Host from './pages/Host/Host';
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/host" element={<Host />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Nav() {
-  return <div>nav</div>;
+  const token = window.localStorage.getItem('TOKEN');
+
+  return (
+    <NavContainer>
+      <NavWrap>
+        <ul>
+          <Link to="/">
+            <img src="/images/logo.jpeg" alt="logo" className="logoImg" />
+          </Link>
+          <NavItem>
+            <Link to="/">남의집 둘러보기</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/host">호스트</Link>
+          </NavItem>
+        </ul>
+        <ul>
+          <NavItem>검색</NavItem>
+          {token ? (
+            <NavItem>로그아웃</NavItem>
+          ) : (
+            <NavItem>
+              <Link to="/login">로그인 / 회원가입</Link>
+            </NavItem>
+          )}
+        </ul>
+      </NavWrap>
+    </NavContainer>
+  );
 }
+const NavContainer = styled.div`
+  padding: 15px;
+  border-bottom: 1px solid #dbdbdb;
+`;
+
+const NavWrap = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1150px;
+  margin: 0 auto;
+
+  ul {
+    display: flex;
+    align-items: center;
+
+    .logoImg {
+      width: 40px;
+    }
+  }
+`;
+
+const NavItem = styled.li`
+  margin-left: 20px;
+  font-weight: ${({ theme }) => theme.weightThin};
+`;
