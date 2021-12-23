b0440722e25b86dcb3e48e458c5d377ac6e5caa1
SangHoon <joa3668@gmail.com>
2021-12-23 20:35:12 +0900

    Modify: 반응형 페이지 구현, 관리페이지 레이아웃 수정

--- src/pages/Host/ManageHost.js
+++ src/pages/Host/ManageHost.js
@@ -25,7 +25,7 @@ export default function ManageHost() {
         <AllProgramsBox>
           <AllProgramHead>전체 남의집</AllProgramHead>
           <ProgramList>
-            {userData ? (
+            {userData && userData.programs.length !== 0 ? (
               userData.programs.map(el => {
                 return (
                   <Program
@@ -38,7 +38,7 @@ export default function ManageHost() {
                 );
               })
             ) : (
-              <p>dodododo</p>
+              <NoProgramP>아직 생성된 남의집이 없습니다.</NoProgramP>
             )}
           </ProgramList>
         </AllProgramsBox>
@@ -46,16 +46,19 @@ export default function ManageHost() {
       </ManageBox>
       <RightBox>
         <ProfileBox>
-          <ProfileImgWrap>
-            <Img src={userData && userData.host.profile_image} alt="" />
-            <EditBtn>
-              <i class="fas fa-pen" />
-            </EditBtn>
-          </ProfileImgWrap>
-          <ProfileName>{userData && userData.host.nickname}</ProfileName>
-          <HostDescription>
-            {userData && userData.host.host_description}
-          </HostDescription>
+          <ProfileHead>호스트 프로필</ProfileHead>
+          <ProfileContent>
+            <ProfileImgWrap>
+              <Img src={userData && userData.host.profile_image} alt="" />
+              <EditBtn>
+                <i class="fas fa-pen" />
+              </EditBtn>
+            </ProfileImgWrap>
+            <ProfileName>{userData && userData.host.nickname}</ProfileName>
+            <HostDescription>
+              {userData && userData.host.host_description}
+            </HostDescription>
+          </ProfileContent>
         </ProfileBox>
         <BannerWrap>
           <BannerImg src="/images/banner1.png" />
@@ -78,6 +81,15 @@ const ManageHostWrap = styled.div`
 
   @media ${props => props.theme.laptop} {
     flex-direction: column;
+    justify-content: flex-start;
+
+    :nth-child(1) {
+      order: 2;
+      border: 2px solid red;
+    }
+    :nth-child(2) {
+      order: 1;
+    }
   }
 `;
 
@@ -86,6 +98,10 @@ const ManageBox = styled.div`
   flex-direction: column;
   margin-right: 30px;
   width: 100%;
+
+  @media ${props => props.theme.laptop} {
+    order: 2;
+  }
 `;
 
 const RightBox = styled.div`
@@ -96,6 +112,7 @@ const RightBox = styled.div`
   @media ${props => props.theme.laptop} {
     max-width: 100%;
     order: 1;
+    margin-bottom: 30px;
   }
 `;
 
@@ -106,9 +123,9 @@ const AllProgramsBox = styled.div`
 `;
 
 const AllProgramHead = styled.h1`
-  padding: 20px;
+  padding: 18px;
   border-bottom: 1px solid #dbdbdb;
-  font-size: 18px;
+  font-size: 16px;
   font-weight: 700;
 `;
 
@@ -116,6 +133,12 @@ const ProgramList = styled.ul`
   display: flex;
   flex-direction: column;
   padding: 30px 20px 10px 20px;
+`;
+
+const NoProgramP = styled.p`
+  text-align: center;
+  color: gray;
+  padding: 20px 0 30px 0;
 `;
 
 const NewProgramBtn = styled(Link)`
@@ -133,23 +156,36 @@ const NewProgramBtn = styled(Link)`
 `;
 
 const ProfileBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 50px 0;
   width: 100%;
   border: 1px solid #dbdbdb;
 `;
 
+const ProfileHead = styled.h1`
+  padding: 18px;
+  border-bottom: 1px solid #dbdbdb;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const ProfileContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+`;
+
 const ProfileImgWrap = styled.div`
   width: 100px;
-  width: 100px;
+  height: 100px;
   margin-bottom: 30px;
   position: relative;
 `;
 
-const HostDescription = styled.p``;
+const HostDescription = styled.p`
+  font-size: 16px;
+  color: gray;
+`;
 
 const EditBtn = styled.div`
   position: absolute;
@@ -180,6 +216,10 @@ const BannerWrap = styled.div`
   padding: 15px 20px 0 20px;
   margin-top: 20px;
   background-color: #f1eee9;
+
+  @media ${props => props.theme.laptop} {
+    display: none;
+  }
 `;
 const BannerImg = styled.img`
   width: 100%;
